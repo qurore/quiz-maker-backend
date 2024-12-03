77c775c1adec153c4df552fe5ce3bae9abc368bc
Ryosuke Shiroshita <admin@qurore.com>
2024-12-03 10:39:55 -0800

    modified port num

--- server.js
+++ server.js
@@ -494,7 +494,7 @@ app.get('/api/wikipedia/:word', async (req, res) => {
 });
 
 // Start the server
-const PORT = 5001;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
