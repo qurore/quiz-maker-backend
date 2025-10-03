752526a7b3ed8dd7caf499059b1c33c66f2d9c19
Ryosuke Shiroshita <admin@qurore.com>
2025-10-03 16:51:34 -0700

    modified hardcording part

--- server.js
+++ server.js
@@ -494,7 +494,7 @@ app.get('/api/wikipedia/:word', async (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
